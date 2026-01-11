43f5f50c4f9100fd5dd7693ce11b767df3b8fceb
vineela-ws <amaranenivineela54@gmail.com>
2026-01-11 07:10:12 +0530

    TestReportsConcept

---
+++ tests/Chapter4/TestReports.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test'
+
+
+test.describe('SmokeTesting', ()=> {
+    test('Test1', async ({ page }) => {
+      await page.goto('https://www.google.com/');
+      await page.getByLabel('Search',{exact:true}).fill('Playwright by testers talk');
+      await page.getByLabel('Search',{exact:true}).press('Enter');
+      await page.getByRole('link', { name: 'Playwright by Testers Talk' }).first().click();
+      await expect(page.getByText('Playwright by Testers Talk ✅').nth(2)).toBeVisible();
+      await expect(page).toHaveTitle('Playwright by testers talk - YouTube');
+
+})
+
+})
+
+test.describe('RegressionTesting', ()=> {
+    test('Test2', async ({ page }) => {
+      await page.goto('https://www.google.com/');
+      await page.getByLabel('Search',{exact:true}).fill('Playwright by testers talk');
+      await page.getByLabel('Search',{exact:true}).press('Enter');
+      await page.getByRole('link', { name: 'Playwright by Testers Talk' }).first().click();
+      await expect(page.getByText('Playwright by Testers Talk ✅').nth(2)).toBeVisible();
+      await expect(page).toHaveTitle('Playwright by testers talk - YouTube');
+
+})
+
+test('Test3', async ({ page }) => {
+      await page.goto('https://www.google.com/');
+      await page.getByLabel('Search',{exact:true}).fill('Playwright by testers talk');
+      await page.getByLabel('Search',{exact:true}).press('Enter');
+      await page.getByRole('link', { name: 'Playwright by Testers Talk' }).first().click();
+      await expect(page.getByText('Playwright by Testers Talk ✅').nth(2)).toBeVisible();
+      await expect(page).toHaveTitle('Playwright by testers talk - YouTube');
+
+})
+
+})
+
+
+
+
