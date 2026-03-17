03c5b267a411180d05747de691a2ad43f88a8f4e
vineela-ws <amaranenivineela54@gmail.com>
2026-03-17 10:11:00 +0530

    config changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -49,6 +49,9 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     actionTimeout:10000,
+    launchOptions : {
+      args:['--start-maximized']
+    }
   },
 
   /* Configure projects for major browsers */
